be2786b26151701bfcee85b42ed2e95528174ad4
lukako2 <71923198+lukako2@users.noreply.github.com>
2023-04-24 15:36:17 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,47 @@
 $(document).ready(function () {
-    $(window).resize(function() {
-        if ($(window).width() >= 800) {
-          $('.ul-large').removeClass('show-menu');
-        } else {
-          $('.show-menu').show();
-        }
-      });
+  $(window).resize(function () {
+    if ($(window).width() >= 850) {
+      $(".ul-large").removeClass("show-menu");
+      $(".nav-a").css("display", "block");
+      $(".logo").removeClass("bigger");
+      $(".nav-a").removeClass("d-block");
+    } else {
+      $(".show-menu").show();
+      $(".nav-a").css("display", "none");
+    }
+  });
 
-    $('.menu-btn').click(function (e) { 
-        $('.ul-large').toggleClass('show-menu anim');
-        // $('nav').css({
-        //     'transition':'all 0.5s'
-        // });
-        $('.logo').toggleClass('bigger');
-    });
+  $(".menu-btn").click(function (e) {
+    $(".ul-large").toggleClass("show-menu anim");
+    $(".logo").toggleClass("bigger");
+    $(".nav-a").toggleClass("d-block");
+  });
+});
+
+let interval;
+
+const element = document.querySelector(".scramble");
+const originalText = element.innerText;
+
+const randomInt = (max) => Math.floor(Math.random() * max);
+const randomFromArray = (array) => array[randomInt(array.length)];
+
+const scrambleText = (text) => {
+  const chars = "*><)c.-:;!".split("");
+  return text
+    .split("")
+    .map((x) => (randomInt(3) > 1 ? randomFromArray(chars) : x))
+    .join("");
+};
+
+element.addEventListener("mouseover", () => {
+  interval = setInterval(
+    () => (element.innerText = scrambleText(originalText)),
+    100
+  );
+});
+
+element.addEventListener("mouseout", () => {
+  clearInterval(interval);
+  element.innerText = originalText;
 });
